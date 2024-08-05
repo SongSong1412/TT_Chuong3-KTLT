ce16d0f3dce7fc5cad4a9e2e518fbe957066d5d0
Le Ngoc Dinh Tri <dinhtri05112003@gmail.com>
2024-08-05 20:36:14 +0700

    finish Chuong3_Bai7_Viet chuong trinh chen 1 tu o bat cu vi tri nao nguoi dung yeu cau

---
+++ Chuong3_KTLT_P2/Bai7.cpp
@@ -0,0 +1,51 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+void ChenTu(char str[], char tu[], int viTri) {
+	char KetQua[100]; 
+	int i, j;
+	int DoDaiStr = strlen(str);
+	int DoDaiTu = strlen(tu);
+
+	// Sao chép phần đầu của chuỗi str vào ketQua
+	for (i = 0; i < viTri; i++) {
+		KetQua[i] = str[i];
+	}
+
+	// Sao chép tu vào ketQua tại vị trí viTri
+	for (j = 0; j < DoDaiTu; j++) {
+		KetQua[i + j] = tu[j];
+	}
+
+	// Sao chép phần còn lại của chuỗi str vào ketQua sau khi chèn tu
+	for (i = viTri; i < DoDaiStr; i++) {
+		KetQua[i + DoDaiTu] = str[i];
+	}
+
+	// Thêm ký tự kết thúc chuỗi
+	KetQua[DoDaiStr + DoDaiTu] = '\0';
+
+	// Sao chép ketQua trở lại str
+	strcpy(str, KetQua);
+}
+
+int main() {
+	char str[200];
+	char tu[50];
+	int viTri;
+
+	printf("Nhap chuoi: ");
+	gets(str);
+
+	printf("Nhap tu can chen: ");
+	gets(tu);
+
+	printf("Nhap vi tri can chen: ");
+	scanf("%d", &viTri);
+
+	ChenTu(str, tu, viTri);
+
+	printf("Chuoi sau khi chen: %s\n", str);
+
+	return 0;
+}
