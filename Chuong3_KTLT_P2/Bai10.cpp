fabfd3cf93cf1dffdad5dacef2e7fdd9033ad0a5
Le Ngoc Dinh Tri <dinhtri05112003@gmail.com>
2024-08-05 22:05:01 +0700

    finish Chuong3_Bai10_Viet chuong trinh tim kiem xem ky tu nao xuat hien nhieu nhat trong chuoi

---
+++ Chuong3_KTLT_P2/Bai10.cpp
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+#define MAX 256
+
+void TimKyTuXuatHienNhieuNhat(char str[]) {
+	int tanSo[MAX] = { 0 }; // Mảng lưu trữ tần số xuất hiện của các ký tự
+	int i;
+	int MaxTanSo = 0; // Biến lưu trữ tần số lớn nhất
+	char KyTuMax; // Ký tự xuất hiện nhiều nhất
+
+	// Tính tần số xuất hiện của từng ký tự
+	for (i = 0; str[i] != '\0'; i++) {
+		tanSo[(unsigned char)str[i]]++;
+	}
+
+	// Tìm ký tự có tần số xuất hiện nhiều nhất
+	for (i = 0; i < MAX; i++) {
+		if (tanSo[i] > MaxTanSo) {
+			MaxTanSo = tanSo[i];
+			KyTuMax = (char)i;
+		}
+	}
+
+	// Hiển thị kết quả
+	if (MaxTanSo > 0) {
+		printf("Ky tu xuat hien nhieu nhat: '%c' voi %d lan\n", KyTuMax, MaxTanSo);
+	}
+	else {
+		printf("Khong co ky tu nao trong chuoi.\n");
+	}
+}
+
+int main() {
+	char str[200];
+
+	printf("Nhap chuoi: ");
+	gets(str);
+
+	TimKyTuXuatHienNhieuNhat(str);
+
+	return 0;
+}
